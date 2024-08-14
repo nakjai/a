bf2c432265a4b95afe4b15ef2c6735ac7e6e56ac
nakjai <nakaji20031212@gmail.com>
2024-08-14 05:58:01 +0000

    エラー解消

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,21 +3,21 @@
     <head>
         <meta charset="utf-8">
         <title>Blog</title>
-
         <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
-    <body class="antialiased">
-        <h1>Blog name</h1>
+    <body>
+        <h1>Blog Name</h1>
         <div class='posts'>
-            @foreach($posts as $post)
-            <div class='post'>
-                <h2 class='title'>{{ $post->title }}</h2>
-                <p class='body'>{{ $post->body }}</p>
+            @foreach ($posts as $post)
+                <div class='post'>
+                    <h2 class='title'>{{ $post->title }}</h2>
+                    <p class='body'>{{ $post->body }}</p>
                 </div>
             @endforeach
+        </div>
+        <div class='paginate'>
+            {{ $posts->links() }}
         </div>
     </body>
 </html>
